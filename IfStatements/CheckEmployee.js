17d4e8cd9b8c46b06544f602a8675d6dfa16af7e
Pranjal Haswani <pranjalhaswani00@gmail.com>
2025-03-05 14:41:26 +0530

    [Pranjal_Haswani]add. Calculate daily employee wage based on part time or full time work.

--- IfStatements/CheckEmployee.js
+++ IfStatements/CheckEmployee.js
@@ -1,13 +1,13 @@
 //UC1
-const IS_ABSENT=0
-let empCheck= Math.floor(Math.random()*10)%2;
-if(empCheck== IS_ABSENT){
-    console.log("Employee is Absent");
-    return;
-}
-else{
-    console.log("Employee is Present")
-}
+// const IS_ABSENT=0
+// let empCheck= Math.floor(Math.random()*10)%2;
+// if(empCheck== IS_ABSENT){
+//     console.log("Employee is Absent");
+//     return;
+// }
+// else{
+//     console.log("Employee is Present")
+// }
 //UC2
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
